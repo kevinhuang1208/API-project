8623b61e2ed79a93668752798cb9570c9ca9185a
Kevin Huang <kevinhuang1208@gmail.com>
2023-04-13 17:39:03 -0400

    Refactored firstName and lastName attributes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -9,6 +9,7 @@ const router = express.Router();
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { UniqueConstraintError } = require('sequelize');
 
 const validateSignup = [
   check('email')
@@ -35,12 +36,14 @@ router.post(
     '/',
     validateSignup,
     async (req, res) => {
-      const { email, password, username } = req.body;
+      const { email, password, username, firstName, lastName } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ email, username, hashedPassword });
+      const user = await User.create({ email, username, hashedPassword, firstName, lastName });
 
       const safeUser = {
         id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email,
         username: user.username,
       };
