3b72729ebd332c8e6c41dfc401eb36799bcbd3e4
Kevin Huang <kevinhuang1208@gmail.com>
2023-04-13 16:52:22 -0400

    Add User signup backend endpoint

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -18,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     username: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
       validate: {
         len: [4, 30],
         isNotEmail(value) {
@@ -30,6 +31,7 @@ module.exports = (sequelize, DataTypes) => {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
       validate: {
         len: [3, 256],
         isEmail: true
