f511e31b7c33e629cc697122a37bda85bfa3e6c2
Kevin Huang <kevinhuang1208@gmail.com>
2023-04-18 12:46:30 -0400

    Get all spots

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,16 @@
+// backend/routes/api/spots.js
+
+const express = require('express');
+
+const { Spot } = require('../../db/models');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const spots = await Spot.findAll()
+
+    res.json(spots)
+})
+
+
+module.exports = router;
