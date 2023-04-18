7c3d6dd180c2cba1747b7d20d2040210e6252bbb
Kevin Huang <kevinhuang1208@gmail.com>
2023-04-18 15:31:16 -0400

    added details for spot endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,14 +2,83 @@
 
 const express = require('express');
 
-const { Spot } = require('../../db/models');
+const { Spot, Review, SpotImage, User } = require('../../db/models');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const spots = await Spot.findAll()
 
-    res.json(spots)
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const spot = await Spot.findByPk(id, {
+        include: [
+        {
+            model: SpotImage,
+            attributes: ['id', 'url', 'preview']
+        },
+        {
+            model: User,
+            attributes: ['id', 'firstName', 'lastName']
+        }
+    ]
+    })
+
+    const numReviews = await Review.findAll({
+        where: {
+            spotId: id
+        }
+    })
+
+    let count = 0;
+
+    for(let perStar of numReviews) {
+        count += perStar.stars
+    }
+
+    const data = {
+        id: spot.id,
+        ownerId: spot.ownerId,
+        address: spot.address,
+        city: spot.city,
+        state: spot.state,
+        country: spot.country,
+        lat: spot.lat,
+        lng: spot.lng,
+        name: spot.name,
+        description: spot.description,
+        price: spot.price,
+        createdAt: spot.createdAt,
+        updatedAt: spot.updatedAt,
+        numReviews: numReviews.length,
+        avgStarRating: count / numReviews.length,
+        SpotImages: spot.SpotImages,
+        Owner: spot.User
+
+    }
+
+
+    if(!spot) {
+        res.status(404)
+        return res.json({
+            message: "Spot couldn't be found"
+        })
+    } else {
+
+    return res.json(data)
+    }
+})
+
+router.get('/', async (req, res) => {
+
+    const Spots = await Spot.findAll({
+        include: {
+            model: Review,
+            attributes: ['stars']
+        }
+    })
+
+    res.json({
+        Spots
+    })
 })
 
 
