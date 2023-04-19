3e00dc92fa02b169d99bc279732f7432344cd35b
Kevin Huang <kevinhuang1208@gmail.com>
2023-04-19 14:14:42 -0400

    added reviews of current user endpoint

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 
-const { Spot, Review, SpotImage, User } = require('../../db/models');
+const { Spot, Review, ReviewImage, User, SpotImage } = require('../../db/models');
 
 const router = express.Router();
 
@@ -12,6 +12,38 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 
+
+//get all reviews of current user
+router.get('/current', requireAuth, async (req, res) => {
+    const reviews = await Review.findAll({
+        include: [
+            {model: User, attributes: {exclude: ['username', 'email', 'hashedPassword', 'createdAt', 'updatedAt']}},
+            {model: Spot, attributes: {exclude: ['createdAt', 'updatedAt']}, include: {model: SpotImage}},
+            {model: ReviewImage, attributes: {exclude: ['createdAt', 'updatedAt', 'reviewId']}}
+        ],
+        where: {
+            userId: req.user.dataValues.id
+        }
+    })
+
+    let Reviews = [];
+
+    reviews.forEach(review => {
+        Reviews.push(review.toJSON())
+    })
+
+
+    Reviews.forEach(data => {
+        data.Spot.SpotImages.forEach(image => {
+            data.Spot.previewImage = image.url
+          })
+          delete data.Spot.SpotImages
+    })
+
+    res.json({
+        Reviews
+    })
+})
 
 
 
