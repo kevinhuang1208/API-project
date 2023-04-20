3ee64a940afce387fc8d711d5c681e3d96c5f8af
Kevin Huang <kevinhuang1208@gmail.com>
2023-04-20 12:27:26 -0400

    added delete review-images endpoint

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -8,6 +8,35 @@ const { Review, ReviewImage } = require('../../db/models');
 
 const router = express.Router();
 
+router.delete('/:id', requireAuth, async (req, res) => {
 
+    const image = await ReviewImage.findByPk(req.params.id,
+        {
+            include: {model: Review}
+        })
+
+
+    if(!image) {
+        res.status(404)
+        return res.json({
+            message: "Spot Image couldn't be found"
+        })
+    }
+
+    if(image.Review.userId !== req.user.dataValues.id) {
+        res.status(403)
+        return res.json({
+            message: 'Forbidden'
+        })
+    }
+
+    if(image) {
+        await image.destroy()
+        res.json({
+            message: "Successfully deleted"
+        })
+    }
+
+})
 
 module.exports = router;
